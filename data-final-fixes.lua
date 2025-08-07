8edb65afce9a019e83049286d69e7483b16a9f80
yaim0425 <yaim0425@gmail.com>
2025-08-07 13:46:52 -0500

    Corrección de la función This_MOD.create_entity

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -29,7 +29,7 @@ function This_MOD.start()
     for _, Space in pairs(This_MOD.info) do
         This_MOD.create_recipe(Space)
         This_MOD.create_item(Space)
-        -- This_MOD.CreateEntity(Space)
+        This_MOD.create_entity(Space)
     end
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
@@ -250,28 +250,28 @@ function This_MOD.create_item(tier)
 end
 
 --- Crear la entidad a usa
-function This_MOD.CreateEntity(tier)
+function This_MOD.create_entity(tier)
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
     ---> Cópiar los valores de la entidad de referencia
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    local entity                  = util.copy(tier.entity)
-    entity.hidden                 = nil
+    local Entity = util.copy(tier.entity)
+    Entity.hidden = nil
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
     ---> Sobre escribir los valores constantes
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    entity.type                   = "loader-1x1"
+    Entity.type = "loader-1x1"
 
-    local fast_replaceable_group  = entity.fast_replaceable_group
-    fast_replaceable_group        = GPrefix.delete_prefix(fast_replaceable_group)
-    entity.fast_replaceable_group = This_MOD.prefix .. fast_replaceable_group
+    local fast_replaceable_group = Entity.fast_replaceable_group
+    fast_replaceable_group = GPrefix.delete_prefix(fast_replaceable_group)
+    Entity.fast_replaceable_group = This_MOD.prefix .. fast_replaceable_group
 
-    entity.filter_count           = 5
-    entity.container_distance     = 1
+    Entity.filter_count = 5
+    Entity.container_distance = 1
 
-    entity.structure              = {
+    Entity.structure = {
         back_patch = {
             sheet = {
                 filename = This_MOD.graphics.entity.back,
@@ -346,7 +346,7 @@ function This_MOD.CreateEntity(tier)
         }
     }
 
-    entity.icons                  = {
+    Entity.icons = {
         { icon = This_MOD.graphics.icon.base },
         { icon = This_MOD.graphics.icon.mask, tint = tier.color },
     }
@@ -355,41 +355,40 @@ function This_MOD.CreateEntity(tier)
     ---> Sobre escribir los valores variables
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    entity.name                   = This_MOD.prefix .. GPrefix.delete_prefix(tier.entity.name)
-    local toFind                  = string.gsub(This_MOD.ref.under, "%-", "%%-")
-    entity.name                   = string.gsub(entity.name, toFind, This_MOD.name)
+    Entity.name = This_MOD.prefix .. GPrefix.delete_prefix(tier.entity.name)
+    Entity.name = string.gsub(Entity.name, This_MOD.ref.to_find, This_MOD.name)
 
-    local localised_name          = { "entity-name." .. This_MOD.prefix .. tier.name .. This_MOD.name }
-    entity.localised_name         = { "", localised_name }
+    local localised_name = { "entity-name." .. This_MOD.prefix .. tier.name .. This_MOD.name }
+    Entity.localised_name = { "", localised_name }
 
-    local localised_description   = { "entity-description." .. This_MOD.prefix .. This_MOD.name }
-    entity.localised_description  = { "", localised_description }
+    local localised_description = { "entity-description." .. This_MOD.prefix .. This_MOD.name }
+    Entity.localised_description = { "", localised_description }
 
-    entity.minable                = {
+    Entity.minable = {
         mining_time = 0.5,
         results = { {
             type = "item",
-            name = entity.name,
+            name = Entity.name,
             amount = 1
         } }
     }
 
-    if entity.next_upgrade then
-        local name = GPrefix.delete_prefix(entity.next_upgrade)
+    if Entity.next_upgrade then
+        local name = GPrefix.delete_prefix(Entity.next_upgrade)
         name = string.gsub(name, "^[0-9%-]+", "")
-        name = string.gsub(name, toFind, "")
+        name = string.gsub(name, This_MOD.ref.to_find, "")
         if This_MOD.info[name] then
-            local next_upgrade  = GPrefix.delete_prefix(entity.next_upgrade)
-            next_upgrade        = string.gsub(next_upgrade, toFind, This_MOD.name)
-            entity.next_upgrade = This_MOD.prefix .. next_upgrade
+            local next_upgrade = GPrefix.delete_prefix(Entity.next_upgrade)
+            next_upgrade = string.gsub(next_upgrade, This_MOD.ref.to_find, This_MOD.name)
+            Entity.next_upgrade = This_MOD.prefix .. next_upgrade
         else
-            entity.next_upgrade = nil
+            Entity.next_upgrade = nil
         end
     else
-        entity.next_upgrade = nil
+        Entity.next_upgrade = nil
     end
 
-    entity.icons                    = {
+    Entity.icons = {
         { icon = This_MOD.graphics.icon.base },
         { icon = This_MOD.graphics.icon.mask, tint = tier.color },
     }
@@ -398,14 +397,16 @@ function This_MOD.CreateEntity(tier)
     ---> Ejemplo en la simulación
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    entity.factoriopedia_simulation = nil
+    Entity.factoriopedia_simulation = nil
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
     ---> Crear el prototipo
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
     --- Crear el prototipo
-    GPrefix.addDataRaw({ entity })
+    GPrefix.extend(Entity)
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 end
 
 ---------------------------------------------------------------------------------------------------
